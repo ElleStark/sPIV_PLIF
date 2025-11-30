aa01fd2cb78a8a78110a80da543926a6b1e6db6b
ElleStark <elle.stark5645@gmail.com>
2025-11-30 05:56:36 -0700

    temp code

--- src/sPIV_PLIF_postprocessing/io/readwrite_imx_to_npy.py
+++ src/sPIV_PLIF_postprocessing/io/readwrite_imx_to_npy.py
@@ -3,52 +3,87 @@ import lvpyio as lv
 import numpy as np
 import matplotlib.pyplot as plt
 
-save_dir = 'I:/PLIF_20Hz_data/processing/'
-save_name = 'data_8.29_30cms_FractalTG15cm_He0.897_air0.917_PIV0.02_iso_L3_combined.npy'
-data_dir = 'I:/PLIF_20Hz_data/data_8.29_30cms_FractalTG15cm_He0.897_air0.917_PIV0.02_iso_L3/'
+# save_dir = 'I:/PLIF_20Hz_data/processing/'
+# save_name = 'data_8.29_30cms_FractalTG15cm_He0.897_air0.917_PIV0.02_iso_L3_combined.npy'
+# data_dir = 'I:/PLIF_20Hz_data/data_8.29_30cms_FractalTG15cm_He0.897_air0.917_PIV0.02_iso_L3/'
 
-# specify list of file paths and new file names
-path1 = data_dir + 'L3.set'
-path2 = data_dir + 'L4.set'
-offset = 0
+# # specify list of file paths and new file names
+# path1 = data_dir + 'L3.set'
+# path2 = data_dir + 'L4.set'
+# offset = 0
 
-# set up buffer objects (see lavision pyio manual)
-s1 = lv.read_set(path1)
-s2 = lv.read_set(path2)
-# check size and n frames
-buffer = s1[0]
-arr = buffer.as_masked_array()
-dims = np.shape(arr.data)
-total_frames = len(s1) + len(s2)  
-total_frames = total_frames - offset  # manually select subset in time if needed
-# total_frames = 10  # for testing
+# # set up buffer objects (see lavision pyio manual)
+# s1 = lv.read_set(path1)
+# s2 = lv.read_set(path2)
+# # check size and n frames
+# buffer = s1[0]
+# arr = buffer.as_masked_array()
+# dims = np.shape(arr.data)
+# total_frames = len(s1) + len(s2)  
+# total_frames = total_frames - offset  # manually select subset in time if needed
+# # total_frames = 10  # for testing
 
-# initialize array for storing collated data
-combined_data = np.zeros((dims[0], dims[1], total_frames), dtype=np.float32)
+# # initialize array for storing collated data
+# combined_data = np.zeros((dims[0], dims[1], total_frames), dtype=np.float32)
 
-# collate images into single sequential stack
-frame_list = [x+offset for x in list(range(total_frames))]
-# frame_list = range(total_frames)
-for i in frame_list:
-    # if even, read from first set, if odd from second set
-    if i%2==0:  
-        buffer = s1[int(i/2)]
-    else:
-        buffer = s2[int((i-1)/2)]
+# # collate images into single sequential stack
+# frame_list = [x+offset for x in list(range(total_frames))]
+# # frame_list = range(total_frames)
+# for i in frame_list:
+#     # if even, read from first set, if odd from second set
+#     if i%2==0:  
+#         buffer = s1[int(i/2)]
+#     else:
+#         buffer = s2[int((i-1)/2)]
 
-    arr = buffer.as_masked_array()  # necessary conversion (see lavision pyio manual)
-    tempdata = arr.data  # check if needed
+#     arr = buffer.as_masked_array()  # necessary conversion (see lavision pyio manual)
+#     tempdata = arr.data  # check if needed
 
-    combined_data[:, :, i-offset] = tempdata
+#     combined_data[:, :, i-offset] = tempdata
 
 
-print(f'combined data dims: {combined_data.shape}')
-# QC plots of a few frames
-for frame in [0, 1, 1000, 1001, 5000, 5001]:
-    plt.imshow(combined_data[:, :, frame], cmap='jet')
-    plt.colorbar()
-    plt.show()  
+# print(f'combined data dims: {combined_data.shape}')
+# # QC plots of a few frames
+# for frame in [0, 1, 1000, 1001, 5000, 5001]:
+#     plt.imshow(combined_data[:, :, frame], cmap='jet')
+#     plt.colorbar()
+#     plt.show()  
 
-# save stack of raw data (if desired)
-save_file = save_dir + save_name
-np.save(save_file, combined_data)
+# # save stack of raw data (if desired)
+# save_file = save_dir + save_name
+# np.save(save_file, combined_data)
+
+########## Convert and save bg and ff calibrated images ############
+source_dir = 'C:/Users/Lavision/Documents/eNose_Processing/ff_bg_davisFiles/ff_bg_calibrated_eNose/'
+save_dir = 'C:/Users/Lavision/Documents/eNose_Processing/ff_bg_davisFiles/ff_bg_calibrated_npy/'
+
+file_list = [
+    'bg_r52_L3/Avg_bg_r52_L3/AddCameraAttributes/bg_r52_L3_calibrated.set',
+    'bg_r52_L3/Avg_bg_r52_L4/AddCameraAttributes/bg_r52_L4_calibrated.set',
+    'bg_r64_L3/Avgbgr64_L3/AddCameraAttributes/bg_r64_L3_calibrated.set',
+    'bg_r64_L3/Avgbgr64_L4/AddCameraAttributes/bg_r64_L4_calibrated.set',
+    'bg_r69_L3/Avg_bgr69_L3/AddCameraAttributes/bg_r69_L3_calibrated.set',
+    'bg_r69_L3/Avg_bgr69_L4/AddCameraAttributes/bg_r69_L4_calibrated.set',
+    'bg_r74_L3/Avg_bg74_L3/AddCameraAttributes/bg_r74_L3_calibrated.set',
+    'bg_r74_L3/Avg_bg74_L4/AddCameraAttributes/bg_r74_L4_calibrated.set',
+    'ff_r63_L3/Avg_ff_r63_L3/AddCameraAttributes/ff_r63_L3_calibrated.set',
+    'ff_r63_L3/Avg_ff_r63_L4/AddCameraAttributes/ff_r63_L4_calibrated.set',
+    'ff_r68_L3/Avg_ffr68_L3/AddCameraAttributes/ff_r68_L3_calibrated.set',
+    'ff_r68_L3/Avg_ffr68_L4/AddCameraAttributes/ff_r68_L4_calibrated.set',
+    'ff_r73_L4/Avg_ff73_L3/AddCameraAttributes/ff_r73_L3_calibrated.set',
+    'ff_r73_L4/Avg_ff73_L4/AddCameraAttributes/ff_r73_L4_calibrated.set',
+    'ff_r78_L3/Avg_ff78_L3/AddCameraAttributes/ff_r78_L3_calibrated.set',
+    'ff_r78_L3/Avg_ff78_L4/AddCameraAttributes/ff_r78_L4_calibrated.set'
+]
+
+for file in file_list:
+    path = source_dir + file
+    s = lv.read_set(path)
+    buffer = s[0]
+    arr = buffer.as_masked_array()
+    tempdata = arr.data
+    tempdata = np.float32(tempdata)
+    save_file = save_dir + file.split('/')[-1].split('.')[0] + '.npy'
+    np.save(save_file, tempdata)
+    print(f'saved {save_file} with dims {tempdata.shape}')
+
